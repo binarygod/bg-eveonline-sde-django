3a6a88838e1a11a0265b9f0d8829b78183936007
Tim Davis <binary.god@gmail.com>
2019-09-21 15:36:26 -0400

    Added SDE Content
    Also added update sde command, not functional yet

---
+++ bg_eveonline_sde_django/common/management/commands/update_sde.py
@@ -0,0 +1,39 @@
+import requests
+import bz2
+import subprocess
+import os
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Updates SDE from Fuzzworks'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        for table in settings.EVEONLINE_SDE_TABLES:
+            self.stdout.write(self.style.NOTICE('Updating {table}...'.format(table=table)))
+            # Download the file
+            r = requests.get('https://www.fuzzwork.co.uk/dump/latest/{table}.sql.bz2'.format(table=table))
+            open('{table}.sql.bz2'.format(table=table), 'wb').write(r.content)
+
+            # Extract the SQL Dump
+            zipfile = bz2.BZ2File('{table}.sql.bz2'.format(table=table))
+            data = zipfile.read()
+            newfilepath = '{table}.sql'.format(table=table)
+            open(newfilepath, 'wb').write(data)
+
+            # Import the SQL Dump
+            p = subprocess.run('mysql -u {user} --password={password} -h {host} {database} < {table}.sql'.format(
+                    user=settings.DATABASES['default']['USER'],
+                    password=settings.DATABASES['default']['PASSWORD'],
+                    host=settings.DATABASES['default']['HOST'],
+                    database=settings.DATABASES['default']['NAME'],
+                    table=table
+            ), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+            # Delete the files
+            os.remove('{table}.sql.bz2'.format(table=table))
+            os.remove('{table}.sql'.format(table=table))
