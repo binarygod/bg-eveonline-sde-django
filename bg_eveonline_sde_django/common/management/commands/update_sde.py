b8a6217231e320e35da60ad401c7c39f6b39fb0c
Tim Davis <binary.god@gmail.com>
2019-09-22 11:33:41 -0400

    - Worked on SDE Update command

--- bg_eveonline_sde_django/common/management/commands/update_sde.py
+++ bg_eveonline_sde_django/common/management/commands/update_sde.py
@@ -13,27 +13,36 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        for table in settings.EVEONLINE_SDE_TABLES:
+
+        tables = []
+
+        app: str
+        for app in settings.INSTALLED_APPS:
+            # Check if starts with bg_eveonline_sde_django
+            if app.startswith('bg_eveonline_sde_django.'):
+                tables.append(app.split('.')[:1])
+
+        for table in tables:
             self.stdout.write(self.style.NOTICE('Updating {table}...'.format(table=table)))
             # Download the file
-            r = requests.get('https://www.fuzzwork.co.uk/dump/latest/{table}.sql.bz2'.format(table=table))
-            open('{table}.sql.bz2'.format(table=table), 'wb').write(r.content)
-
-            # Extract the SQL Dump
-            zipfile = bz2.BZ2File('{table}.sql.bz2'.format(table=table))
-            data = zipfile.read()
-            newfilepath = '{table}.sql'.format(table=table)
-            open(newfilepath, 'wb').write(data)
-
-            # Import the SQL Dump
-            p = subprocess.run('mysql -u {user} --password={password} -h {host} {database} < {table}.sql'.format(
-                    user=settings.DATABASES['default']['USER'],
-                    password=settings.DATABASES['default']['PASSWORD'],
-                    host=settings.DATABASES['default']['HOST'],
-                    database=settings.DATABASES['default']['NAME'],
-                    table=table
-            ), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-            # Delete the files
-            os.remove('{table}.sql.bz2'.format(table=table))
-            os.remove('{table}.sql'.format(table=table))
+            # r = requests.get('https://www.fuzzwork.co.uk/dump/latest/{table}.sql.bz2'.format(table=table))
+            # open('{table}.sql.bz2'.format(table=table), 'wb').write(r.content)
+            #
+            # # Extract the SQL Dump
+            # zipfile = bz2.BZ2File('{table}.sql.bz2'.format(table=table))
+            # data = zipfile.read()
+            # newfilepath = '{table}.sql'.format(table=table)
+            # open(newfilepath, 'wb').write(data)
+            #
+            # # Import the SQL Dump
+            # p = subprocess.run('mysql -u {user} --password={password} -h {host} {database} < {table}.sql'.format(
+            #         user=settings.DATABASES['default']['USER'],
+            #         password=settings.DATABASES['default']['PASSWORD'],
+            #         host=settings.DATABASES['default']['HOST'],
+            #         database=settings.DATABASES['default']['NAME'],
+            #         table=table
+            # ), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            #
+            # # Delete the files
+            # os.remove('{table}.sql.bz2'.format(table=table))
+            # os.remove('{table}.sql'.format(table=table))
